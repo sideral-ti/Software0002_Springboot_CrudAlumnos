80bfd3aa2f5a1f6019b1f70946c9bb6a6730b53c
Sideral TI <sideralti.appgmail.com>
2024-10-27 01:20:42 -0300

    primer commit, new Branch -> dev

---
+++ src/main/java/cl/sideralti/app/Software0002SpringbootListaDeAlumnosCrudApplication.java
@@ -0,0 +1,13 @@
+package cl.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Software0002SpringbootListaDeAlumnosCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Software0002SpringbootListaDeAlumnosCrudApplication.class, args);
+	}
+
+}
